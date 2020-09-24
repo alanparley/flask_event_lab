fd5a56c39fd8e1acfa9dc126016ef67202f0c7f7
Alan Parley <alanparley@hotmail.com>
2020-09-24 15:18:51 +0100

    inital commit

---
+++ app/controller.py
@@ -0,0 +1,8 @@
+from flask import render_template
+from app import app
+from app.models.event_list import events
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Home', events=events)
+
